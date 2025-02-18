e16da06f154e9e94e7c732ca847fed8f56a66a2f
ahmetdidarzafer <ahmetdidarzafer@gmail.com>
2025-02-18 20:04:42 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS options
 const corsOptions = {
-  origin: 'https://ozkara.onrender.com/',
+  origin: 'https://ozkara.onrender.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
